422e79e2e6b88b652bce2742197a7b1d365abe13
Niels Gregers Johansen <niels@timeboxer.dk>
2025-02-10 11:28:01 +0100

    fix

--- components/my-tools-list-server.tsx
+++ components/my-tools-list-server.tsx
@@ -2,15 +2,7 @@ import prisma from "@/prisma";
 import { MyToolList2 } from "./my-tool-list";
 
 export async function MyToolListServer(props: { searchFor?: string; onLoaded?: () => void }) {
-  const tools = (await prisma.tool.findMany({
-    where: {
-      // name: {
-      //   contains: props.query,
-      //   mode: "insensitive"
-      // }
-    }
 
-  })).sort((a, b) => a.name.localeCompare(b.name));
 
   return <div>server
     {/* <MyToolList2 searchFor={props.searchFor} onLoaded={props.onLoaded} /> */}
